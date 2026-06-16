8f14253553d1ce50841a80165828c0cffe16b7d4
ThalexS2008 <thalexsbr@gmail.com>
2026-06-15 21:24:51 -0300

    entrada de dados pelo terminal e operacoes matemáticas

---
+++ programa02.py
@@ -0,0 +1,12 @@
+nome = input ("Digite seu nome: ")
+nome = input ("Digite o nome do seu melhor amigo:")
+
+print(f"Ola {nome} seja bem vindo ao meu programa!")
+
+ano_nasc = int( input(f"Prazer em te conhecer {nome}"))
+idade = input (f"Prazer em te conhecer {nome}, me fala a sua idade: ")
+
+idade = 2026 - ano_nasc
+print(f"Parabéns por seus {idade} anos")
+
+print(f"O seu melhor amigo é o {nome_amigo}")
---
+++ programa03.py
@@ -0,0 +1,28 @@
+# Calculadora python
+
+num1 = int(input("Digite o primeiro número: "))
+
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+divisao_inteira = num1//num2
+resto_divisao = num1 % num2
+potenciacao = num1 ** num2
+
+print(f"A soma de {num1} e {num2} é igual a = {soma}")
+soma = num1 + num2
+print(f"A subtracao de {num1} e {num2} é igual a = {subtracao}")
+subtracao = num1 - num2
+print(f"A multiplicacao de {num1} e {num2} é igual a = {multiplicacao}")
+multiplicacao = num1 * num2
+print(f"A divisao de {num1} e {num2} é igual a = {divisa:.2f}")
+divisao = num1 / num2
+print(f'A divisao_inteira de {num1} e {num2} é igual a = {divisao_inteira}')
+divisao_inteira = num1 // num2
+print(f"A resto_divisao de {num1} e {num2} é igual a = {resto_divisao}")
+resto_divisao = num1 % num2
+print(f"A potenciacao de {num1} e {num2} é igual a = {potenciacao}")
+potenciacao = num1 ** num2
